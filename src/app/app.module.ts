6e99bcccf5e149894d07274dc80b403922eb14ba
samwendo <wendosam21@gmail.com>
2019-10-02 10:56:51 +0300

    made changes in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FormsModule } from '@angular/forms';
+import { LastNamePipe } from './last-name.pipe';
+import { DirectDirective } from './direct.directive';
+import {RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'home', component: ProfileComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    LastNamePipe,
+    DirectDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
