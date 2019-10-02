511cd748242b05cd1daab651374aeacf919b50dd
samwendo <wendosam21@gmail.com>
2019-10-02 10:50:28 +0300

    added a repository file

---
+++ src/app/repository.ts
@@ -0,0 +1,7 @@
+export class Repository {
+  constructor(public name,
+              // tslint:disable-next-line:variable-name
+              public html_url,
+              public description
+              ) {}
+}
